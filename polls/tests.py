410e2354bea2096785be4dd0de2cb1c813d24304
Emmar Caber <caberemmar@gmail.com>
2024-01-04 19:59:48 +0800

    feat: testing a view

--- polls/tests.py
+++ polls/tests.py
@@ -1,12 +1,44 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 import datetime
 
 
+def create_question(question_text, days):
+    """
+    Create a question with the given "question_text" and published the
+    given_number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question=question_text, pub_date=time)
+
+
 # Create your tests here.
 class QuestionModelTestCase(TestCase):
+    def test_no_question(self):
+        """
+        If no question exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Question with a pub_date in the past are displayed on the
+        index page.
+        """
+        question = create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
