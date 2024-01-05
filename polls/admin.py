baa8dd0c4248e800839453bd13fed27f7a8814cb
Emmar Caber <caberemmar@gmail.com>
2024-01-05 10:00:24 +0800

    feat: customize admin view

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]})
+    ]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
