d5c933ead260c484bd8777ec00f792217b21c81a
Emmar Caber <caberemmar@gmail.com>
2024-01-05 10:04:19 +0800

    feat: add related objects
    Customize admin where question and choices can be edited in one view

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date Information", {"fields": ["pub_date"]})
     ]
+    inlines = [ChoiceInline]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
